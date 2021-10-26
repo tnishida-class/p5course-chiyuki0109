91409c146462a5c0083881c7318595cc24719345
chiyuki0109 <chiyuki00109@gmail.com>
2021-10-26 14:52:56 +0900

    Change section 2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  for(let i = 0; i < 5; i++){
+  noFill();
+    stroke(255, 0, 0);
+    let x = i * 10 + 10;
+    ellipse(50, 50, x);
+  }
+  for(let i = 5; i < 10; i++){
+  noFill();
+    stroke(0, 0, 255);
+    let x = i * 10 + 10;
+    ellipse(50, 50, x);
   }
 }
